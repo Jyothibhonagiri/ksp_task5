086c589d6bc8eedc54415bbd2c48b1b266c8ce1d
Jyothibhonagiri <142475505+Jyothibhonagiri@users.noreply.github.com>
2023-09-15 12:33:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+window.alert("hi welcome");
+// it show alert pop up on the screen//
+let a=5;
+let b=6;
+console.log(a);
+console.log(b);
+console.log(c=a+b);
+//a and b are variables,5 and 6 are values//
+let ageOfAPerson=20;//camelcase variable//
+const number4=10;
+console.log(number4);
+//you can't change the variables in constant//
+let number1=10;
+let number2=20;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+//arithmetic operators + - * / %//
+let sample=6;
+console.log(sample=6);
+console.log(sample+=1);
+console.log(sample*=1);
+console.log(sample-=1);
+console.log(sample/=3);
+console.log(sample%=4);
+//assignment operators = += *= /= %= //
+let sample1=7;
+let sample2=6;
+console.log(sample1==sample2);
+console.log(sample1!=sample2);
+console.log(sample1>sample2);
+console.log(sample1<sample2);
+console.log(sample1>=sample2);
+console.log(sample1<=sample2);
+//comparision operator == != >= <= > < //
+let string="full stack";
+console.log(string.length);
+console.log(string.slice());
+console.log(string.substring());
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+console.log(string.replace());
+console.log(string.trim());
+console.log(string.split());
+console.log(string.trimStart());
+let firstname="harry";
+let lastname="potter";
+console.log(firstname.concat(lastname));
+// string methods//
+let demo1=55;// number datatype//
+let demo2="55";//string datatype//
+console.log(demo1===demo2);
+console.log(demo1);
+console.log(demo2);
